1845b5f7778fdb05175a8ccfecabc1e130c951c8
Xavier Dutoit <github@tttp.eu>
2017-04-26 18:13:36 +0200

    with a new html generator for emails

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var jsfiles = [
 'node_modules/jquery/dist/jquery.js',
 'node_modules/bootstrap/dist/js/bootstrap.js',
 'node_modules/vue/dist/vue.js',
+'node_modules/vue-form/dist/vue-form.js',
 'node_modules/clipboard-js/clipboard.js',
 ];
 var cssfiles = [
