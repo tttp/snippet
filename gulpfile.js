f7d20eb3aa5acefef4e5caad89aaf53b07fee39f
Xavier Dutoit <github@tttp.eu>
2017-04-27 16:02:00 +0200

    with partial partials

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var cleanCSS = require('gulp-clean-css');
+var inject = require('gulp-inject');
+var mode = require('gulp-mode')();
 
 var DEST = './dist/';
 var jsfiles = [
@@ -14,12 +16,33 @@ var jsfiles = [
 'node_modules/vue-form/dist/vue-form.js',
 'node_modules/clipboard-js/clipboard.js',
 ];
+
 var cssfiles = [
 'node_modules/bootstrap/dist/css/bootstrap.css',
 'node_modules/bootstrap/dist/css/bootstrap-theme.css',
 ];
 //node_modules/bootstrap/dist/css/bootstrap
 //node_modules/jquery/dist/jquery.js
+
+gulp.task('partial', function() {
+gulp.src('index.html')
+  .pipe(inject(gulp.src(['partial/head.html']), {
+    starttag: '<!-- inject:head:{{ext}} -->',
+    transform: function (filePath, file) {
+      // return file contents as string 
+      return file.contents.toString('utf8')
+    }
+  }))
+  .pipe(inject(gulp.src(['partial/nav.html']), {
+    starttag: '<!-- inject:nav:{{ext}} -->',
+    transform: function (filePath, file) {
+      // return file contents as string 
+      return file.contents.toString('utf8')
+    }
+  }))
+  .pipe(gulp.dest('./dist'));
+});
+
 
 gulp.task('copy', function() {
   gulp.src(jsfiles)
@@ -40,6 +63,8 @@ gulp.task('minify-css', ['copy'], function() {
 
 gulp.task('minify-js', ['copy'],function() {
 //gulp.src(DEST+'js/*.js')
+//.pipe(mode.production(uglify()))
+
 return gulp.src(jsfiles)
     .pipe(concat('all.js'))
     .pipe(gulp.dest(DEST))
